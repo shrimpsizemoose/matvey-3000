036c475e6c16aef8837c1d1663bfa7c875f50cfb
Igor Mosyagin <408698+cra@users.noreply.github.com>
2023-04-05 15:10:08 +0200

    Add /pic command

--- src/bot_handler.py
+++ src/bot_handler.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
-import random
 import collections
+import logging
 import os
 import pathlib
-import logging
+import random
 from dataclasses import dataclass
 
 import openai
@@ -69,10 +69,39 @@ async def dump_message_info(message: types.Message):
     await message.reply(message.chat.id)
 
 
+@dp.message_handler(commands=['pic'])
+async def gimme_pic(message: types.Message):
+    _, prompt = message.get_full_command()
+    await message.chat.do('upload_photo')
+    try:
+        response = openai.Image.create(
+            prompt=prompt,
+            n=1,
+            size='512x512',
+        )
+    except openai.error.InvalidRequestError:
+        messages_to_send = config.setup[:]
+        messages_to_send.append(
+            {
+                'role': 'user',
+                'content': f'объясни трагикомичной шуткой почему OpenAI не может сгенерировать картинку по запросу "{prompt}"',
+            }
+        )
+        await message.chat.do('typing')
+        try:
+            response = openai.ChatCompletion.create(model=config.model, messages=messages_to_send)
+        except openai.error.RateLimitError as e:
+            await message.answer(f'Кажется я подустал и откнулся в рейт-лимит. Давай сделаем перерыв ненадолго.\n\n{e}')
+        else:
+            await message.answer(response['choices'][0]['message']['content'])
+    else:
+        await message.chat.do('upload_photo')
+        image_url = response['data'][0]['url']
+        await message.answer_photo(image_url)
+
+
 @dp.message_handler(lambda message: message.chat.id in config.allowed_chat_id)
 async def send_chatgpt_response(message: types.Message):
-    ##print(message)
-
     # if last message is a single word, ignore it
     args = message.parse_entities()
     args = args.split()
@@ -80,11 +109,14 @@ async def send_chatgpt_response(message: types.Message):
         return
 
     message_chain = extract_message_chain(message, bot.id)
-
+    # print(message_chain)
     if not any(msg['role'] == 'assistant' for msg in message_chain):
         if len(message_chain) > 1 and random.random() < 0.95:
-            # podpizdnut mode guard
+            # podpizdnut mode
+            print('uuf')
             return
+
+    print('hello?')
     if len(message_chain) == 1 and message.chat.id < 0:
         if not any(config.me in x for x in args):
             # nobody mentioned me, so I shut up
@@ -101,8 +133,12 @@ async def send_chatgpt_response(message: types.Message):
     messages_to_send.extend(message_chain)
 
     await message.chat.do('typing')
-    response = openai.ChatCompletion.create(model=config.model, messages=messages_to_send)
-    await message.reply(response['choices'][0]['message']['content'])
+    try:
+        response = openai.ChatCompletion.create(model=config.model, messages=messages_to_send)
+    except openai.error.RateLimitError as e:
+        await message.answer(f'Кажется я подустал и воткнулся в рейт-лимит. Давай сделаем перерыв ненадолго.\n\n{e}')
+    else:
+        await message.reply(response['choices'][0]['message']['content'])
 
 
 if __name__ == '__main__':
